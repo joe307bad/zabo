23644bcdcf0e54e4f192d442ad0651a9be9943ec
joe307bad <joe307bad@gmail.com>
2022-01-03 00:50:13 -0500

    [WIP] downgrading to nx 12

--- .storybook/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  stories: [
-    '../src/lib/**/*.stories.mdx',
-    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: [ '@nrwl/react/plugins/storybook'],
-};
--- libs/react-native-expo/src/utils/ensure-node-modules-symlink.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function ensureNodeModulesSymlink(workspaceRoot: string, projectRoot: string): void;
